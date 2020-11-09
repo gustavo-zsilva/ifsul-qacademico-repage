593463ae34e0e9ae72a91fcff12cd9b917863aa8
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2020-11-09 20:16:17 -0300

    First Commit

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Landing() {
+    return (
+        <div className="landing">
+            <header>
+                <h1>
+                    
+                </h1>
+            </header>
+        </div>
+    );
+}
+
+export default Landing;
